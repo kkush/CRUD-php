5c56a2c79745b2a8b71de72ccc74d14579c51815
Thomas Creeten <thomas@creeation.de>
2014-09-03 02:23:24 +0200

    + add utf-8 support

--- database.php
+++ database.php
@@ -14,7 +14,7 @@
     	if ( null == self::$cont ) 
     	{ 
     		try
-    		 { self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); }
+    		 { self::$cont = new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); }
     		  catch(PDOException $e) 
  { 
     		die($e->getMessage()); } } return self::$cont; } public static function disconnect() { self::$cont = null; } } ?> 
